439fd35dd225295a5bbc143609343daec99654ab
hoangdong1996 <hoangdongfit@gmai.com>
2020-08-06 17:14:50 +0700

    tach data

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -1,29 +1,43 @@
 <template>
-    <div class="newsletter-section section-space--ptb_120 default-bg" id="contact">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-md-10  ml-auto mr-auto">
-                    <div class="section-title-wrapper text-center section-space--mb_50 wow move-up">
-                        <h3 class="section-title text-white">493 businesses signed up <br> last week. Join them!</h3>
-                    </div>
-                    <!-- newsletter form -->
-                    <div class="newsletter-form--one section-space--mb_20 text-center wow move-up">
-                        <form action="#">
-                            <input type="text" placeholder="Your name">
-                            <input type="email" placeholder="Email">
-                            <button class="btn btn--white btn-text-theme">Subscribe</button>
-                        </form>
-                    </div>
-                    <!-- newsletter text -->
-                    <h6 class="text-white text-center wow move-up">What’s next in AeroLand? <a href="#" class="hover-style-link hover-style-link--green">Learn more</a></h6>
-                </div>
-            </div>
+  <div class="newsletter-section section-space--ptb_120 default-bg" id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 ml-auto mr-auto">
+          <div class="section-title-wrapper text-center section-space--mb_50 wow move-up">
+            <h3 class="section-title text-white" v-html="title"></h3>
+          </div>
+          <!-- newsletter form -->
+          <div class="newsletter-form--one section-space--mb_20 text-center wow move-up">
+            <form action="#">
+              <input type="text" placeholder="Your name" />
+              <input type="email" placeholder="Email" />
+              <button class="btn btn--white btn-text-theme">Subscribe</button>
+            </form>
+          </div>
+          <!-- newsletter text -->
+          <h6 class="text-white text-center wow move-up">
+            {{text}}
+            <a
+              v-bind:href="textHref"
+              class="hover-style-link hover-style-link--green"
+            >{{buttonTitle}}</a>
+          </h6>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'newsletter'
-    }
+export default {
+  name: "newsletter",
+  data() {
+    return {
+      title: "493 businesses signed up <br/> last week. Join them!",
+      text: "What’s next in AeroLand?",
+      textHref: "#",
+      buttonTitle: "Learn more",
+    };
+  },
+};
 </script>
--- src/components/ProgressCircle.vue
+++ src/components/ProgressCircle.vue
@@ -1,92 +1,158 @@
 <template>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="feature-list-progress section-space--pt_60">
-                <div class="feature-list-progress__counter">
-                    <div class="circle-progress-wrapper">
-                        <div class="chart-progress">
-                            <div class="chart-progress__inner-wrapper">
-                                <div class="chart-progress__inner">
-                                    <vue-circle
-                                        :progress="90"
-                                        :size="160"
-                                        line-cap="round"
-                                        :fill="fill"
-                                        :thickness="4"
-                                        :start-angle="0"
-                                        :show-percent="true">
-                                    </vue-circle>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="circle-design one"></div>
-                        <div class="circle-design two"></div>
-                    </div>
+  <div class="row">
+    <div class="col-md-6" v-for="(progress, index) in fearureListProgress" :key="index">
+      <div class="feature-list-progress section-space--pt_60">
+        <div class="feature-list-progress__counter">
+          <div class="circle-progress-wrapper">
+            <div class="chart-progress">
+              <div class="chart-progress__inner-wrapper">
+                <div class="chart-progress__inner " v-bind:class="progress.class2">
+                  <vue-circle
+                    :progress="progress.progress"
+                    :size="160"
+                    line-cap="round"
+                    :fill="progress.fill"
+                    :thickness="4"
+                    :start-angle="0"
+                    :show-percent="true"
+                  ></vue-circle>
                 </div>
-                <div class="feature-list-progress__info">
-                    <h6 class="feature-list-progress__title mb-20">Digital Marketing</h6>
-                    <ul class="feature-list-progress__list">
-                        <li>Keyword rank tracking</li>
-                        <li>Website audit</li>
-                        <li>Marketing, SEO</li> 
-                        <li>Social Media</li>
-                        <li>Social Media</li>
-                        <li>On-page SEO Audit</li>
-                    </ul>
-                </div>
+              </div>
             </div>
+            <div class="circle-design one" v-bind:class="progress.class"></div>
+            <div class="circle-design two"  v-bind:class="progress.class"></div>
+          </div>
         </div>
-        <div class="col-md-6">
-            <div class="feature-list-progress section-space--pt_60">
-                <div class="feature-list-progress__counter">
-                    <div class="circle-progress-wrapper">
-                        <div class="chart-progress">
-                            <div class="chart-progress__inner-wrapper">
-                                <div class="chart-progress__inner chart-progress__inner--blue">
-                                    <vue-circle
-                                        :progress="75"
-                                        :size="160"
-                                        line-cap="round"
-                                        :fill="blue"
-                                        :thickness="4"
-                                        :start-angle="0"
-                                        :show-percent="true">
-                                    </vue-circle>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="circle-design circle-design--blue one"></div>
-                        <div class="circle-design circle-design--blue two"></div>
-                    </div>
-                </div>
-                <div class="feature-list-progress__info">
-                    <h6 class="feature-list-progress__title mb-20">Digital Marketing</h6>
-                    <ul class="feature-list-progress__list">
-                        <li>Digital Marketing</li>
-                        <li>Website audit</li>
-                        <li>Marketing, SEO</li>
-                        <li>Social Media</li>
-                        <li>Social Media</li>
-                        <li>On-page SEO Audit</li>
-                    </ul>
-                </div>
-            </div>
+        <div class="feature-list-progress__info">
+          <h6 class="feature-list-progress__title mb-20">{{progress.title}}</h6>
+          <ul
+            class="feature-list-progress__list"
+            v-for="(list) in progress.listProgress"
+            :key="list"
+          >
+            <li>{{list}}</li>
+          </ul>
         </div>
+      </div>
     </div>
+
+    <!-- <div class="col-md-6">
+      <div class="feature-list-progress section-space--pt_60">
+        <div class="feature-list-progress__counter">
+          <div class="circle-progress-wrapper">
+            <div class="chart-progress">
+              <div class="chart-progress__inner-wrapper">
+                <div class="chart-progress__inner">
+                  <vue-circle
+                    :progress="fearureListProgress.progress[0]"
+                    :size="160"
+                    line-cap="round"
+                    :fill="fill"
+                    :thickness="4"
+                    :start-angle="0"
+                    :show-percent="true"
+                  ></vue-circle>
+                </div>
+              </div>
+            </div>
+            <div class="circle-design one"></div>
+            <div class="circle-design two"></div>
+          </div>
+        </div>
+        <div class="feature-list-progress__info">
+          <h6 class="feature-list-progress__title mb-20">{{fearureListProgress.title[0]}}</h6>
+          <ul class="feature-list-progress__list">
+            <li>Keyword rank tracking</li>
+            <li>Website audit</li>
+            <li>Marketing, SEO</li>
+            <li>Social Media</li>
+            <li>Social Media</li>
+            <li>On-page SEO Audit</li>
+          </ul>
+        </div>
+      </div>
+    </div>-->
+    <!-- <div class="col-md-6">
+      <div class="feature-list-progress section-space--pt_60">
+        <div class="feature-list-progress__counter">
+          <div class="circle-progress-wrapper">
+            <div class="chart-progress">
+              <div class="chart-progress__inner-wrapper">
+                <div class="chart-progress__inner chart-progress__inner--blue">
+                  <vue-circle
+                    :progress="fearureListProgress.progress[1]"
+                    :size="160"
+                    line-cap="round"
+                    :fill="blue"
+                    :thickness="4"
+                    :start-angle="0"
+                    :show-percent="true"
+                  ></vue-circle>
+                </div>
+              </div>
+            </div>
+            <div class="circle-design circle-design--blue one"></div>
+            <div class="circle-design circle-design--blue two"></div>
+          </div>
+        </div>
+        <div class="feature-list-progress__info">
+          <h6 class="feature-list-progress__title mb-20">{{fearureListProgress.title[1]}}</h6>
+          <ul class="feature-list-progress__list">
+            <li>Digital Marketing</li>
+            <li>Website audit</li>
+            <li>Marketing, SEO</li>
+            <li>Social Media</li>
+            <li>Social Media</li>
+            <li>On-page SEO Audit</li>
+          </ul>
+        </div>
+      </div>
+    </div>-->
+  </div>
 </template>
 
 <script>
-    import VueCircle from 'vue2-circle-progress/src/index.vue'
+import VueCircle from "vue2-circle-progress/src/index.vue";
 
-    export default {
-        components: {
-            VueCircle
+export default {
+  components: {
+    VueCircle,
+  },
+  data() {
+    return {
+      fearureListProgress: [
+        {
+          progress: 80,
+          title: "Digital Marketing",
+          fill: { gradient: ["#fad961", "#f76b1c"] },
+          class: "",
+          class2: "",
+          listProgress: [
+            "Digital Marketing",
+            "Website audit",
+            "Marketing, SEO",
+            "Social Media",
+            "On-page  Audit",
+            "On-page SEO Audit",
+          ],
         },
-        data (){
-            return {
-                fill : { gradient: ["#fad961", "#f76b1c"] },
-                blue : { gradient: ["#9C7AF2", "#5E61E7"] },
-            }
-        }
-    }
+        {
+          progress: 90,
+          title: "Digital Marketing",
+          blue: { gradient: ["#9C7AF2", "#5E61E7"] },
+          class: "circle-design--blue",
+          class2: "chart-progress__inner--blue",
+          listProgress: [
+            "Digital Marketing",
+            "Website audit",
+            "Marketing, SEO",
+            "Social Media",
+            "On-SEO Audit",
+            "On-page SEO ",
+          ],
+        },
+      ],
+    };
+  },
+};
 </script>
