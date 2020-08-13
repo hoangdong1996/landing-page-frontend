2ac2c667e513a66ddb0bbbbb051ec1f0156f183b
hoangdong1996 <hoangdongfit@gmai.com>
2020-08-13 08:36:51 +0700

    navbar form

--- src/components/FormInput/NavbarForm.vue
+++ src/components/FormInput/NavbarForm.vue
@@ -1,49 +1,23 @@
 <template>
-  <div>
-    <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai"></el-option>
-          <el-option label="Zone two" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="Pick a date" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker placeholder="Pick a time" v-model="form.date2" style="width: 100%;"></el-time-picker>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type"></el-checkbox>
-          <el-checkbox label="Promotion activities" name="type"></el-checkbox>
-          <el-checkbox label="Offline activities" name="type"></el-checkbox>
-          <el-checkbox label="Simple brand exposure" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor"></el-radio>
-          <el-radio label="Venue"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input type="textarea" v-model="form.desc"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button>Cancel</el-button>
-      </el-form-item>
-    </el-form>
+  <div >
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>Navbar</span>
+        <el-button style="float: right; padding: 3px 0" type="text"></el-button>
+      </div>
+      <el-form ref="form" :model="form" label-width="120px">
+        <el-upload
+            class="upload-demo"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :on-preview="handlePreview"
+            :on-remove="handleRemove"
+            :file-list="fileList"
+            list-type="picture">
+          <el-button size="small" type="primary">Click to upload</el-button>
+          <div slot="tip" class="el-upload__tip">jpg/png files with a size less than 500kb</div>
+        </el-upload>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -72,5 +46,26 @@ export default {
 </script>
 
 <style scoped>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both
+}
+
+.upload-demo el-upload-list--picture el-upload-list__item{
+  width: 300px;
+}
+
 
 </style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -26,38 +26,43 @@
             background-color="#545c64"
             text-color="#fff"
             active-text-color="#ffd04b">
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>Navigator One</span>
-            </template>
-          </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span>Navigator Two</span>
+
+          <el-menu-item index="1" :route="{ path:'navbar-form'}">
+            <i class="el-icon-edit"></i>
+            <span>Navbar </span>
           </el-menu-item>
 
-<!--          <router-link to="/dashboard/navbar-form">-->
-            <el-menu-item index="3" >
-              <i class="el-icon-document"></i>
-              <span>Navigator Three</span>
-            </el-menu-item>
-<!--          </router-link>-->
-
-          <el-menu-item index="4" :route="{ path:'about-section-form'}">
-            <i class="el-icon-setting"></i>
-            <span>About r</span>
-          </el-menu-item>
-
-          <el-menu-item index="5" :route="{ path:'navbar-form'}">
-            <i class="el-icon-setting"></i>
-            <span>Navbar r</span>
+          <el-menu-item index="2" :route="{ path:'about-section-form'}">
+            <i class="el-icon-edit"></i>
+            <span>About Section</span>
           </el-menu-item>
 
         </el-menu>
       </el-aside>
+
       <el-container>
-        <router-view></router-view>
+        <el-header>
+          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+            <el-menu-item index="1">Processing Center</el-menu-item>
+            <el-submenu index="2">
+              <template slot="title">Workspace</template>
+              <el-menu-item index="2-1">item one</el-menu-item>
+              <el-menu-item index="2-2">item two</el-menu-item>
+              <el-menu-item index="2-3">item three</el-menu-item>
+              <el-submenu index="2-4">
+                <template slot="title">item four</template>
+                <el-menu-item index="2-4-1">item one</el-menu-item>
+                <el-menu-item index="2-4-2">item two</el-menu-item>
+                <el-menu-item index="2-4-3">item three</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="3" disabled>Info</el-menu-item>
+            <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
+          </el-menu>
+        </el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
       </el-container>
     </el-container>
   </div>
