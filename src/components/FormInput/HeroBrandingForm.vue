f8217eb983b9b4335cb28059c6517b66a47992d2
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-08-18 14:51:39 +0700

    fix herobranding

--- src/components/FormInput/HeroBrandingForm.vue
+++ src/components/FormInput/HeroBrandingForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-card class="box-card">
+    <el-card class="box-card" >
       <div slot="header" class="clearfix">
         <span>Hero Branding</span>
       </div>
@@ -19,16 +19,16 @@
         </el-form-item>
         <el-form-item label="Background">
           <el-upload
-              accept="image/*"
-              name="files"
-              ref="upload"
-              class="upload-demo upload"
-              action="http://192.168.1.122:8081/api/image/uploadFile"
-              :file-list="fileList "
-              :auto-upload="false"
-              list-type="picture"
-              :limit="1"
-              :on-success="handleSuccess"
+            accept="image/*"
+            name="files"
+            ref="upload"
+            class="upload-demo upload"
+            action="http://192.168.1.122:8081/api/image/uploadFile"
+            :file-list="fileList "
+            :auto-upload="false"
+            list-type="picture"
+            :limit="1"
+            :on-success="handleSuccess"
           >
             <el-button size="small" type="primary">Click to upload</el-button>
           </el-upload>
@@ -40,16 +40,41 @@
         </el-form-item>
       </el-form>
     </el-card>
+    <el-card>
+      <section
+        id="home"
+        class="hero-branding bg-cover"
+        style="position: relative"
+        :style="{ 'background-image': 'url(data:image/png;base64,' + preview.background_img.data +')' }"
+      >
+        <div class="container-fluid container-fluid--cp-150">
+          <div class="hero-branding">
+            <div class="hero-content">
+              <h2 class="h1 hero-content-title">{{ preview.title }}</h2>
+              <h6 class="hero-content-subtitle mt-20">{{ preview.description }}</h6>
+              <div class="slider-button mt-30">
+                <router-link
+                  class="ht-btn ht-btn-md"
+                >{{ preview.button_title }}</router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </el-card>
   </div>
 </template>
 
 <script>
-import {mapGetters} from "vuex";
-import {createHeroBranding} from "@/api/heroBranding";
+import { mapGetters } from "vuex";
+import { createHeroBranding } from "@/api/heroBranding";
 
 export default {
   computed: {
     ...mapGetters(["heroBranding"]),
+    preview(){
+      return {...this.heroBranding}
+    }
   },
   data() {
     return {
@@ -57,8 +82,8 @@ export default {
     };
   },
   async mounted() {
-    await this.$store.dispatch("heroBranding/getHeroBranding")
-    this.heroBranding.id = null
+    await this.$store.dispatch("heroBranding/getHeroBranding");
+    this.heroBranding.id = null;
   },
   methods: {
     onSubmit() {
@@ -69,7 +94,7 @@ export default {
         title: "Success",
         message: "This is a success message",
         type: "success",
-      })
+      });
     },
     errorNotify() {
       this.$notify({
@@ -78,18 +103,22 @@ export default {
       });
     },
     handleSuccess(response) {
-      this.heroBranding.background_img = response.data
-      createHeroBranding(this.heroBranding).then(() => {
-        this.successNotify()
-      }).catch(() => {
-        this.errorNotify()
-      })
+      this.heroBranding.background_img = response.data;
+      createHeroBranding(this.heroBranding)
+        .then(() => {
+          this.successNotify();
+        })
+        .catch(() => {
+          this.errorNotify();
+        });
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/scss/_variabls.scss";
+@import "@/assets/scss/elements/_hero-branding.scss";
 .upload {
   border: 1px gainsboro solid;
   border-radius: 5px;
@@ -112,4 +141,5 @@ export default {
   padding: 10px 0;
   background-color: #f9fafc;
 }
+
 </style>
