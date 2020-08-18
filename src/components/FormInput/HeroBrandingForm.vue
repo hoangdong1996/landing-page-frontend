a97ebec8d4feed03cb522186dfbca4619098b627
hoangdong1996 <hoangdongfit@gmai.com>
2020-08-18 12:01:25 +0700

    fix heroBranding section form

--- src/components/FormInput/HeroBrandingForm.vue
+++ src/components/FormInput/HeroBrandingForm.vue
@@ -4,7 +4,7 @@
       <div slot="header" class="clearfix">
         <span>Hero Branding</span>
       </div>
-      <el-form ref="form" :model="heroBranding" label-width="120px" >
+      <el-form ref="form" :model="heroBranding" label-width="120px">
         <el-form-item label="Title">
           <el-input class="input-label" v-model="heroBranding.title"></el-input>
         </el-form-item>
@@ -28,7 +28,7 @@
               :auto-upload="false"
               list-type="picture"
               :limit="1"
-              :on-success="handleSuccess"
+              :on-change="handleChange"
           >
             <el-button size="small" type="primary">Click to upload</el-button>
           </el-upload>
@@ -46,6 +46,8 @@
 <script>
 import {mapGetters} from "vuex";
 import {createHeroBranding} from "@/api/heroBranding";
+import {uploadFile} from "@/api/upload";
+import {errorNotify, successNotify} from "@/function/notify";
 
 export default {
   computed: {
@@ -54,46 +56,40 @@ export default {
   data() {
     return {
       fileList: [],
+      imageSection: null,
+      resImageSection: null
     };
+  },
+  methods: {
+    handleChange(file) {
+      this.imageSection = file.raw
+    },
+    async onSubmit() {
+      await this.uploadFile()
+      this.submitFormRequest()
+    },
+    async uploadFile() {
+      if (this.imageSection !== null) {
+        await uploadFile(this.imageSection).then(res => {
+          this.resImageSection = res.data.data
+        }).catch(() => this.resetAll())
+      }
+    },
+    resetAll() {
+      this.imageSection = null
+      this.resImageSection = null
+    },
+    submitFormRequest() {
+      if (this.resImageSection !== null) {
+      this.heroBranding.background_img = this.resImageSection
+      }
+      createHeroBranding(this.heroBranding)
+          .then(() => successNotify(this))
+          .catch(() => errorNotify(this))
+    },
   },
   async mounted() {
     await this.$store.dispatch("heroBranding/getHeroBranding");
-    // this.fileList[0] = this.heroBranding.background_img
-    // console.log(this.fileList)
-  },
-  methods: {
-    onSubmit() {
-      this.$refs.upload.submit();
-    },
-    successNotify() {
-      this.$notify({
-        title: "Success",
-        message: "This is a success message",
-        type: "success",
-      })
-    },
-    errorNotify() {
-      this.$notify({
-        title: "Error",
-        message: "error",
-      });
-    },
-    handleSuccess(response) {
-      const requestForm = {
-        title: this.heroBranding.title,
-        description: this.heroBranding.description,
-        button_title: this.heroBranding.button_title,
-        button_href: this.heroBranding.button_href,
-        background_img: {
-          id: response.data[0].id,
-        },
-      };
-      createHeroBranding(requestForm).then(() => {
-        this.successNotify()
-      }).catch(() => {
-        this.errorNotify()
-      })
-    },
   },
 };
 </script>
