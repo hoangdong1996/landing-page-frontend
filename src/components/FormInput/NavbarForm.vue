8ef11677e87f0683112bc56876bac402ed51cf79
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-08-18 12:00:35 +0700

    add navbar preview and upload

--- src/components/FormInput/NavbarForm.vue
+++ src/components/FormInput/NavbarForm.vue
@@ -12,62 +12,104 @@
             name="files"
             ref="upload"
             class="upload-demo upload"
-            action="http://192.168.1.122:8081/api/image/uploadMultiFile"
+            action="http://192.168.1.122:8081/api/image/uploadFile"
             :file-list="fileList"
             :auto-upload="false"
             list-type="picture"
+            :multiple="false"
             :limit="1"
-            :on-success="handleSuccess"
+            :on-change="handleChange"
           >
             <el-button size="small" type="primary">Click to upload</el-button>
           </el-upload>
         </el-form-item>
         <el-form-item style="margin-top: 20px">
-          <el-button type="primary" @click="onSubmit">Create</el-button>
-          <el-button>Cancel</el-button>
+          <el-button type="primary" @click="onSubmit">Save</el-button>
+          <el-button @click.prevent="onPreview" :disabled="dis">Preview</el-button>
         </el-form-item>
       </el-form>
+    </el-card>
+    <el-card class="box-card">
+      <Navbar :navbar="preview" style="position: relative;" />
     </el-card>
   </div>
 </template>
 
 <script>
 import { createNavbar } from "@/api/navbar";
-
+import { mapGetters } from "vuex";
+import { successNotify, errorNotify } from "@/function/notify";
+import Navbar from "@/components/Navbar";
+import { uploadFile } from "@/api/upload";
+import { getBase64 } from "@/function/data";
 export default {
+  name: "navbar-form",
+  components: {
+    Navbar,
+  },
   data() {
     return {
       form: {},
       fileList: [],
       response: null,
+      image: null,
+      imageRes: null,
+      dis: false
     };
   },
-  methods: {
-    onSubmit() {
-      this.$refs.upload.submit();
-    },
-    handleSuccess(response) {
-      this.response = response;
-      const navbar = {
-        logo_src: {
-          id: response.data[0].id,
-        },
-      };
-      createNavbar(navbar).then(() => {
-        this.$notify({
-          title: "Success",
-          message: "This is a success message",
-          type: "success",
-        }).catch(() => {
-          this.$notify.error({
-            title: "Error",
-            message: "error",
-          })
-        })
-      })
+  computed: {
+    ...mapGetters(["navbar"]),
+    preview() {
+      return { ...this.navbar };
     },
   },
-}
+  async mounted() {
+    await this.$store.dispatch("navbar/getLogoNavbar");
+    this.navbar.id = null;
+  },
+  methods: {
+    async onSubmit() {
+      this.dis = true
+      await this.upload();
+      this.submitForm();
+    },
+    async submitForm() {
+      if (this.imageRes === null || this.imageRes === undefined) {
+        return;
+      }
+      this.navbar.logo_src = this.imageRes;
+      await createNavbar(this.navbar)
+        .then(() => {
+          successNotify(this);
+        })
+        .catch(() => {
+          this.resetAll();
+          errorNotify(this);
+        });
+      this.$store.dispatch("navbar/getLogoNavbar");
+      this.dis = false
+    },
+    resetAll() {
+      this.image = null;
+      this.imageRes = null;
+    },
+    handleChange(file) {
+      this.image = file.raw;
+    },
+    async upload() {
+      await uploadFile(this.image).then((response) => {
+        this.imageRes = response.data.data;
+      });
+    },
+    async onPreview() {
+      if (this.image != null) {
+        await getBase64(this.image).then((data) => {
+          this.preview.logo_src.data = data;
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -84,5 +126,9 @@ export default {
 }
 .clearfix:after {
   clear: both;
+}
+
+.preview {
+  position: relative;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,5 @@
 <template>
   <b-navbar
-    v-if="navbar"
     v-b-scrollspy:nav-scroller
     type="light"
     toggleable="xl"
@@ -11,20 +10,34 @@
     <div class="container-fluid container-fluid--cp-150">
       <b-navbar-toggle target="nav_collapse" />
       <b-navbar-brand class="navbar-brand" to="/">
-        <img :src="navbar.logo_src.data | pngSrc" alt="logo">
+        <img v-if="navbar.logo_src" :src="navbar.logo_src.data | pngSrc" alt="logo" />
       </b-navbar-brand>
       <b-btn-group class="header-config-wrapper">
-        <b-btn class="header-config" @click="toggleClass('addClass', 'active')"><i class="far fa-search" /></b-btn>
+        <b-btn class="header-config" @click="toggleClass('addClass', 'active')">
+          <i class="far fa-search" />
+        </b-btn>
         <b-link class="ht-btn ht-btn--outline hire-btn d-none d-xl-block">Hire Us Now</b-link>
       </b-btn-group>
       <b-collapse id="nav_collapse" class="default-nav justify-content-center" is-nav>
         <b-navbar-nav class="navbar-nav main-menu">
-          <b-nav-item to="/"><span>LANDING</span></b-nav-item>
-          <b-nav-item href="#home" class="scroll"><span>HOME</span></b-nav-item>
-          <b-nav-item href="#about" class="scroll"><span>ABOUT</span></b-nav-item>
-          <b-nav-item href="#requirement" class="scroll"><span>REQUIREMENTS</span></b-nav-item>
-          <b-nav-item href="#pricing" class="scroll"><span>PRICING</span></b-nav-item>
-          <b-nav-item href="#partner" class="scroll"><span>PARTNERS</span></b-nav-item>
+          <b-nav-item to="/">
+            <span>LANDING</span>
+          </b-nav-item>
+          <b-nav-item href="#home" class="scroll">
+            <span>HOME</span>
+          </b-nav-item>
+          <b-nav-item href="#about" class="scroll">
+            <span>ABOUT</span>
+          </b-nav-item>
+          <b-nav-item href="#requirement" class="scroll">
+            <span>REQUIREMENTS</span>
+          </b-nav-item>
+          <b-nav-item href="#pricing" class="scroll">
+            <span>PRICING</span>
+          </b-nav-item>
+          <b-nav-item href="#partner" class="scroll">
+            <span>PARTNERS</span>
+          </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </div>
@@ -32,13 +45,13 @@
 </template>
 
 <script>
-
 export default {
-  name: 'Navbar',
+  name: "Navbar",
   props: {
     navbar: {
       type: Object,
-      default: null
+      default: null,
+      required: true
     },
   },
   data() {
@@ -47,69 +60,74 @@ export default {
       limitPosition: 200,
       scrolled: false,
       lastPosition: 500,
-    }
+    };
   },
   mounted() {
-    
-
-    (function() {
-      scrollTo()
-    })()
+    (function () {
+      scrollTo();
+    })();
 
     function scrollTo() {
-      const links = document.querySelectorAll('.scroll > a')
-      links.forEach(each => (each.onclick = scrollAnchors))
+      const links = document.querySelectorAll(".scroll > a");
+      links.forEach((each) => (each.onclick = scrollAnchors));
     }
 
     function scrollAnchors(e, respond = null) {
-      const distanceToTop = el => Math.floor(el.getBoundingClientRect().top)
-      e.preventDefault()
-      var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href')
-      const targetAnchor = document.querySelector(targetID)
-      if (!targetAnchor) return
-      const originalTop = distanceToTop(targetAnchor)
-      window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' })
-      const checkIfDone = setInterval(function() {
-        const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 190
+      const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
+      e.preventDefault();
+      var targetID = respond
+        ? respond.getAttribute("href")
+        : this.getAttribute("href");
+      const targetAnchor = document.querySelector(targetID);
+      if (!targetAnchor) return;
+      const originalTop = distanceToTop(targetAnchor);
+      window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+      const checkIfDone = setInterval(function () {
+        const atBottom =
+          window.innerHeight + window.pageYOffset >=
+          document.body.offsetHeight - 190;
         if (distanceToTop(targetAnchor) === 0 || atBottom) {
-          targetAnchor.tabIndex = '-1'
-          targetAnchor.focus()
-          clearInterval(checkIfDone)
+          targetAnchor.tabIndex = "-1";
+          targetAnchor.focus();
+          clearInterval(checkIfDone);
         }
-      }, 800)
+      }, 800);
     }
   },
   created() {
-    window.addEventListener('scroll', this.handleScroll)
+    window.addEventListener("scroll", this.handleScroll);
   },
   destroyed() {
-    window.removeEventListener('scroll', this.handleScroll)
+    window.removeEventListener("scroll", this.handleScroll);
   },
 
   methods: {
     // sticky menu script
     handleScroll() {
-      if (this.lastPosition < window.scrollY && this.limitPosition < window.scrollY) {
-        this.scrolled = true
+      if (
+        this.lastPosition < window.scrollY &&
+        this.limitPosition < window.scrollY
+      ) {
+        this.scrolled = true;
         // move up!
       }
       if (this.lastPosition > window.scrollY) {
-        this.scrolled = true
+        this.scrolled = true;
         // move down
       }
-      this.lastPosition = window.scrollY
-      this.scrolled = window.scrollY > 50
+      this.lastPosition = window.scrollY;
+      this.scrolled = window.scrollY > 50;
     },
 
     // offcanvas searchbox
     toggleClass(addRemoveClass, className) {
-      const el = document.querySelector('#search-overlay')
-      if (addRemoveClass === 'addClass') {
-        el.classList.add(className)
+      const el = document.querySelector("#search-overlay");
+      if (addRemoveClass === "addClass") {
+        el.classList.add(className);
       } else {
-        el.classList.remove(className)
+        el.classList.remove(className);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
